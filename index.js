63aa5ad632f7dd80f0f9341ef3e208903df9a336
peter <pngugi.wainaina@gmail.com>
2023-08-02 01:16:51 +0300

    my functions continued lab submission

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function saturdayFun(task="roller-skate") {
+    return `This Saturday, I want to ${task}!`
+}
+
+
+
+function mondayWork(task="go to the office") {
+    return `This Monday, I will ${task}.`
+}
+
+
+function wrapAdjective(string="*") {
+    return function(intel="special") {
+        return `You are ${string}${intel}${string}!`
+    }
+}
+
+
+
